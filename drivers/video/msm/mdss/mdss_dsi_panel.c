e85836f5080185dc6e6ad250478724e1081c692e
zhaocheng_wei <zhaochengw@qq.com>
2017-01-01 23:00:12 +0800

    Revert "video: mdss: LiveDisplay driver"
    This reverts commit ca5e9aa16b913f11e36140b113d5c7bec5900c5f.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -29,8 +29,6 @@
 #include "ztemt_disp_preference.h"
 #endif
 
-#include "mdss_livedisplay.h"
-
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -158,7 +156,7 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds)
 {
 	struct dcs_cmd_req cmdreq;
@@ -622,12 +620,6 @@ static int mdss_dsi_set_col_page_addr(struct mdss_panel_data *pdata,
 		}
 	}
 
-#ifdef CONFIG_YULONG_COLOR
-	color_enhancement_impl_apply();
-#endif
-
-	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
-
 end:
 	return 0;
 }
@@ -893,7 +885,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -1715,8 +1707,6 @@ static int mdss_dsi_parse_panel_features(struct device_node *np,
 			pr_err("%s:%d, Disp_en gpio not specified\n",
 					__func__, __LINE__);
 	}
-
-	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 }
